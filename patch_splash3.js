391922bffc2ab7f85c3e85d9f295cac927a6a99d
Abdullah Asim <abdullah333asim@gmail.com>
2026-07-18 22:20:33 +0500

    chore: remove legacy patch scripts
    Delete unused patch files used for modifying codebase components during development.

--- patch_splash3.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('src/components/SplashAnimation.tsx', 'utf-8');
-
-code = code.replace(/x: 155/g, 'x: 145');
-
-fs.writeFileSync('src/components/SplashAnimation.tsx', code);
